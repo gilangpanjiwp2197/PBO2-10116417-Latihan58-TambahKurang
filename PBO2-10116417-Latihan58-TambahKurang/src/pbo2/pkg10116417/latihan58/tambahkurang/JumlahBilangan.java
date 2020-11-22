00b009390755b30bb5654a6dfaafe4b60f72e227
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-22 12:37:20 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan58-TambahKurang/src/pbo2/pkg10116417/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan58.tambahkurang;
+
+/**
+ *
+ * 
+ */
+public class JumlahBilangan extends Bilangan{
+    
+    public void tampilHasilJumlah(){
+        System.out.println("Hasil Perjumlahan = "+ (getX()+getY()));
+    }
+    
+    
+}
---
+++ PBO2-10116417-Latihan58-TambahKurang/src/pbo2/pkg10116417/latihan58/tambahkurang/PBO210116417Latihan58TambahKurang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan58.tambahkurang;
+
+/**
+ *
+ *  NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM PERTAMBAHAN DAN PENGURANGAN
+ */
+public class PBO210116417Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       JumlahBilangan bil = new JumlahBilangan();
+       bil.tampilHasilJumlah();
+       
+       SelisihBilangan blg = new SelisihBilangan();
+       blg.tampilSelisih();
+    
+    }
+    
+}
